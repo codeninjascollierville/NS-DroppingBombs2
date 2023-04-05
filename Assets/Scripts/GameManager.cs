bb79ee750dce0d6f24f3eae0c0baa2d85ebeda50
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-04-05 18:26:37 -0500

    fite nite it feddy

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private FallingBombs Spawner;
+
+    void Awake()
+    {
+        Spawner = GameObject.Find("Spawner").GetComponent<FallingBombs>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       Spawner.active = false; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.anyKeyDown)
+       {
+        Spawner.active = true;
+       }
+    }
+}
